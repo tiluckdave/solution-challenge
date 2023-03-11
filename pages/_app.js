adcc4eff0120da148eeb627a5ffca8e1a8db7b64
Isha Bule <ishabule753@gmail.com>
2023-03-12 01:22:14 +0530

    form added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '/components/nav'
+
 
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Navbar/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
