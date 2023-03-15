6a2a09b8f415efc8a1b2a0f4a860ca54eeeff099
Isha Bule <ishabule753@gmail.com>
2023-03-15 20:09:08 +0530

    Guidlines added

--- components/Guidlines.js
+++ components/Guidlines.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import {Button, useDisclosure, UnorderedList, ListItem } from '@chakra-ui/react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+const Guidlines = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
 
-export const Guidlines = () => {
-  return (
-    <div>Guidlines</div>
-  )
+    return (
+        <>
+
+            <Button onClick={onOpen}>How to share</Button>
+
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>This platform if for you only, feel free to express your suffering.
+                        Just make sure you follow the basic guidlines </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        
+                        <UnorderedList>
+                            <ListItem p='5px'>Entering your age is required</ListItem>
+                            <ListItem p='5px'>Focus on specific actions or behaviors that have affected you.</ListItem>
+                            <ListItem p='5px'>Use clear and concise language to describe the situation.</ListItem>
+                            <ListItem p='5px'>Stick to the facts and avoid exaggeration or hyperbole.</ListItem>
+                            {/* <ListItem p='5px'>You are free to use hate-speech which w</ListItem> */}
+                            <ListItem p='5px'>Use "I" statements to express how you feel and make sure you don't mention the name of any person, use specific pronouns instead.</ListItem>
+                        </UnorderedList>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3}  onClick={onClose}>Ok</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+
 }
+export default Guidlines
--- components/StoryForm.js
+++ components/StoryForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import {
     Button,
     FormControl,
@@ -11,7 +12,11 @@ import {
     Stack,
     Text,
     useNumberInput,
+    Checkbox, useDisclosure
+
 } from '@chakra-ui/react';
+import Guidlines from "@/components/Guidlines";
+
 
 export default function StoryForm() {
     const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
@@ -21,58 +26,65 @@ export default function StoryForm() {
             min: 1,
             max: 99,
         })
+    const [isChecked, setIsChecked] = useState(false);
 
+    const { isOpen, onToggle } = useDisclosure()
     const inc = getIncrementButtonProps()
     const dec = getDecrementButtonProps()
     const input = getInputProps()
-
     return (
-        <Flex
-            w={'full'}
-            align={'center'}
-            justify={'center'}>
-            <Stack
-                spacing={4}
+        <>
+            <Flex
                 w={'full'}
-                maxW={'lg'}
-                bg='white'
-                rounded={'xl'}
-                boxShadow={'lg'}
-                p={6}
-                my={12}>
-                <Heading lineHeight={1.1} textAlign='center' fontSize={{ base: '2xl', md: '3xl' }}>
-                    Share your Story
-                </Heading>
-                <Text textAlign='center'
-                    fontSize={{ base: 'sm', sm: 'md' }}
-                    color='gray.800'>
-                    Shhh! Its Anonymous 🤫
-                </Text>
-                <FormControl id="age" my={8}>
-                    <FormLabel>Enter your Age</FormLabel>
-                    <HStack w='full'>
-                        <Button {...dec}>-</Button>
-                        <Input {...input} />
-                        <Button {...inc}>+</Button>
-                    </HStack>
-                </FormControl>
-                <FormControl id="story" my={8}>
-                    <FormLabel>Free your mind, share what you can&apos;t speak to anyone</FormLabel>
-                    <Textarea rows={10}
-                        placeholder='Type here...' />
-                </FormControl>
+                align={'center'}
+                justify={'center'}
+            
+            >
 
-                <Stack spacing={6}>
-                    <Button
-                        bg={'blue.400'}
-                        color={'white'}
-                        _hover={{
-                            bg: 'blue.500',
-                        }}>
-                        Share
-                    </Button>
+                <Stack
+                    spacing={4}
+                    w={'full'}
+                    maxW={'lg'}
+                    bg='white'
+                    rounded={'xl'}
+                    boxShadow={'lg'}
+                    p={6}
+                    my={12}>
+                    <Heading lineHeight={1.1} textAlign='center' fontSize={{ base: '2xl', md: '3xl' }}>
+                        Share your Story
+                    </Heading>
+                    <Text textAlign='center'
+                        fontSize={{ base: 'sm', sm: 'md' }}
+                        color='gray.800'>
+                        Shhh! Its Anonymous 🤫
+                    </Text>
+                    <FormControl id="age" my={8}>
+                        <FormLabel>Enter your Age</FormLabel>
+                        <HStack w='full'>
+                            <Button {...dec}>-</Button>
+                            <Input {...input} />
+                            <Button {...inc}>+</Button>
+                        </HStack>
+                    </FormControl>
+                    <FormControl id="story" my={8}>
+                        <FormLabel>Free your mind, share what you can&apos;t speak to anyone</FormLabel>
+                        <Textarea rows={10}
+                            placeholder='Type here...' />
+                    </FormControl>
+
+
+                    <Guidlines />
+                    <Checkbox
+                        isChecked={isChecked}
+                        onChange={() => setIsChecked(!isChecked)}
+                    >
+                      "I have read all the guidlines before sharing"
+                    </Checkbox>
+                    <Button isDisabled={!isChecked}  colorScheme='whatsapp'>Share</Button>
                 </Stack>
-            </Stack>
-        </Flex>
+
+            </Flex>
+
+        </>
     );
 }
